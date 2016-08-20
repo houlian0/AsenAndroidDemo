9f81befa8ff1669e96c8493b94eb1e810dd96ccd
Asen <597356831@qq.com>
2016-08-20 12:37:34 +0800

    Initial commit

---
+++ app/src/main/java/com/asen/android/demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.asen.android.demo;
+
+import android.os.Bundle;
+
+import com.asen.android.lib.base.module.activity.BaseActivity;
+
+/**
+ * @author Asen
+ * @version v1.0
+ * @date 2016/8/20 12:20
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
