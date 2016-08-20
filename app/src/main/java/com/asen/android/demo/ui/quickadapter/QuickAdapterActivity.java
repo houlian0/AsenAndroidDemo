2ff7850005752bd6eb82c7204f2931c214b8c9e4
Asen <597356831@qq.com>
2016-08-20 15:55:58 +0800

    新增代码

---
+++ app/src/main/java/com/asen/android/demo/ui/quickadapter/QuickAdapterActivity.java
@@ -0,0 +1,20 @@
+package com.asen.android.demo.ui.quickadapter;
+
+import android.os.Bundle;
+
+import com.asen.android.lib.base.module.activity.BaseActivity;
+
+/**
+ * 快速设配器页面
+ *
+ * @author Asen
+ * @version v1.0
+ * @date 2016/8/20 15:54
+ */
+public class QuickAdapterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
